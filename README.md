4a77e5eff1a7720269e3bcdff8ac43ae0b7a97eb
Nikola-Ver <nikolveresh@gmail.com>
2021-06-26 04:10:36 +0300

    Added support for dynamic hints

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # GitHub-Search
 
-It's a simple Chrome extension for quick searches. To use it, you can write gh in the search bar and write the search name or name/ or name/repository.
+It's a simple Chrome extension for quick searches. To use it, you can write gh in the search bar and write the search name or name/ or name/repository. There are also hints for previous entries. To remove an unnecessary hint, write del before the desired expression. Example: del github/.
--- src/background.js
+++ src/background.js
@@ -1,7 +1,18 @@
+let storage = localStorage.getItem('storage')
+  ? JSON.parse(localStorage.getItem('storage'))
+  : [];
+
 chrome.omnibox.onInputEntered.addListener((text) => {
   chrome.tabs.query({ active: true, currentWindow: true }, ([tab]) => {
     text = text.replace(' ', '+');
+    const deleteVal = text.match(/del\+|.+/g);
     let url = '';
+
+    if (deleteVal && deleteVal[0] === 'del+') {
+      storage = storage.filter((e) => e !== deleteVal[1]);
+      localStorage.setItem('storage', JSON.stringify(storage));
+      return;
+    }
 
     if (text.match(/\//g)) {
       url = `https://github.com/${text}`;
@@ -9,15 +20,21 @@ chrome.omnibox.onInputEntered.addListener((text) => {
       url = `https://github.com/search?q=${text}`;
     }
 
+    if (storage.findIndex((e) => e === text) === -1) {
+      storage.push(text);
+      localStorage.setItem('storage', JSON.stringify(storage));
+    }
+
     chrome.tabs.update(tab.id, { url });
   });
 });
 
 chrome.omnibox.onInputChanged.addListener((text, suggest) => {
-  suggest([
-    {
-      content: '[ search ]   [ name/ ]   [ name/repository ]',
-      description: '[ search ]   [ name/ ]   [ name/repository ]',
-    },
-  ]);
+  const suggestArr = storage
+    .filter((e) => e.indexOf(text) > -1)
+    .map((e) => {
+      return { content: e, description: e };
+    });
+
+  suggest(suggestArr);
 });
